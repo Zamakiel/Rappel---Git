c253e5be09886ea52c27199a2fda9ed42d2c438c
GALANORPC\el0ma <albrod88@gmail.com>
2024-12-07 22:33:22 +0100

    Fixed issues with player event. Added text mesh pro

---
+++ Assets/Scripts/UI/LifeDisplayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LifeDisplayer : MonoBehaviour
+{
+    public GlobalInt currentHealth;
+    public GlobalInt totalHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
